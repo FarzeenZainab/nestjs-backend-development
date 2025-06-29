2d2e8b80fca298aecf67c689c9774a7cd8f9cbbc
FarzeenZainab <farzeenzaidi51@gmail.com>
2025-06-29 22:51:25 +0500

    working with routes inside the controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('tasks') // Each controller should have a route prefix
 export class TasksController {
@@ -6,5 +6,13 @@ export class TasksController {
   public findAll(): string[] {
     // this method returns an array of strings
     return ['A', 'B', 'C', 'D', 'E'];
+  }
+
+  @Get('/:id') // method decorator to handle dynamic routes
+  // The ':id' part of the route is a placeholder for a dynamic parameter
+  // The value of the "id" parameter will be passed to the method as part of the params object
+  // We have to use the @Param() decorator to access the dynamic parameter
+  public findOne(@Param() params: any): string {
+    return `The number is ${params.id}`;
   }
 }
